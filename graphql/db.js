c5479199a49342a6495943a466579c43406be761
knulps <dhlee@thecheat.co.kr>
2018-07-30 19:17:21 +0900

    Creating first Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,25 +1,49 @@
-export const people = [
+let movies = [
   {
-    id: "1",
-    name: "knulps",
-    age: 18,
-    gender: "female"
+    id: 0,
+    name: "Star Wars - The new one",
+    score: 1
   },
   {
-    id: "2",
-    name: "Jane",
-    age: 18,
-    gender: "female"
+    id: 1,
+    name: "Avengers - The new one",
+    score: 8
   },
   {
-    id: "3",
-    name: "Musket",
-    age: 12,
-    gender: "male"
+    id: 2,
+    name: "The Godfather I",
+    score: 99
+  },
+  {
+    id: 3,
+    name: "Logan",
+    score: 2
   }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === String(id));
-  return filteredPeople[0];
+  const filteredMovies = movies.filter(movie => movie.id === String(id));
+  return filteredMovies[0];
+};
+
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
